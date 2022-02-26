4cf275ccad51bd26ca6a00eef03a39efcd7215e1
Gabriel Chammas <gabrielchammas23@gmail.com>
2022-02-27 01:54:54 +0200

    Add id for houses

--- houses.js
+++ houses.js
@@ -1,11 +1,13 @@
 const houses = [
     {
+        id: 1,
         picture: '/img/1.jpg',
         type: 'Entire house',
         town: 'New York',
         title: 'Beautiful flat in New York!'
       },
       {
+        id: 2,
         picture: '/img/2.jpg',
         type: 'Entire house',
         town: 'Amsterdam',
