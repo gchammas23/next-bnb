5971ca013553fab81088e3bb1997eb48883de7fb
Gabriel Chammas <gabrielchammas23@gmail.com>
2022-02-27 19:18:02 +0200

    Add HTML and CSS for login modal

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -1,3 +1,14 @@
 export default function LoginModal(props) {
-    return <p>Login Modal</p>
+    return (
+        <>
+            <h2>Log in</h2>
+            <div>
+                <form>
+                    <input id='email' type='email' placeholder='Email address'/>
+                    <input id='password' type='password' placeholder='Password'/>
+                    <button>Log in</button>
+                </form>
+            </div>
+        </>
+    )
 }
