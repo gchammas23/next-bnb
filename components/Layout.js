5aa3285a88d075ad09901565e37bc7d0f52c183f
Gabriel Chammas <gabrielchammas23@gmail.com>
2022-02-27 10:21:33 +0200

    Add header to Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,9 @@
+import Header from "./Header";
+
 export default function Layout(props) {
     return (
         <div>
+            <Header />
             <main>{props.content}</main>
             <style jsx>{`
                 main {
