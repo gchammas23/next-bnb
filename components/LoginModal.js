af0f1616bcac466ae531b8f5ec8d571598f2c230
Gabriel Chammas <gabrielchammas23@gmail.com>
2022-02-27 19:28:12 +0200

    Add HTML for sign up and add links

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -7,6 +7,11 @@ export default function LoginModal(props) {
                     <input id='email' type='email' placeholder='Email address'/>
                     <input id='password' type='password' placeholder='Password'/>
                     <button>Log in</button>
+
+                    <p>
+                        Don't have an account? {' '}
+                        <a href='javascript:;' onClick={() => props.showSignUp() }></a>
+                    </p>
                 </form>
             </div>
         </>
--- components/RegistrationModal.js
+++ components/RegistrationModal.js
@@ -1,3 +1,20 @@
 export default function RegistrationModal(props) {
-    return <p>Registration Modal</p>
+    return (
+        <>
+            <h2>Sign up</h2>
+            <div>
+                <form>
+                    <input id='email' type='email' placeholder='Email address'/>
+                    <input id='password' type='password' placeholder='Password'/>
+                    <input id='confirmPassword' type='password' placeholder='Confirm your password'/>
+                    <button>Sign up</button>
+
+                    <p>
+                        Already have an account? {' '}
+                        <a href='javascript:;' onClick={() => props.showLogin()}></a>
+                    </p>
+                </form>
+            </div>
+        </>
+    )
 }
