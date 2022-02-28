0b64cc8129494e4656cd911652d52e8ec7e152c3
Gabriel Chammas <gabrielchammas23@gmail.com>
2022-02-28 21:02:28 +0200

    Add state in registration modal

--- components/RegistrationModal.js
+++ components/RegistrationModal.js
@@ -1,15 +1,36 @@
+import { useState } from 'react';
+
 export default function RegistrationModal(props) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleEmailChange = (e) => {
+        e.preventDefault();
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordChange = (e) => {
+        e.preventDefault();
+        setPassword(e.target.value);
+    }
+
+    const handleConfirmPasswordChange = (e) => {
+        e.preventDefault();
+        setConfirmPassword(e.target.value);
+    }
+
     return (
         <>
             <h2>Sign up</h2>
             <div>
                 <form onSubmit={(event) => {
                     event.preventDefault();
-                    alert('Sign up!');
+                    alert(`Email: ${email} Password: ${password} Confirm Password: ${confirmPassword}`);
                 }}>
-                    <input id='email' type='email' placeholder='Email address'/>
-                    <input id='password' type='password' placeholder='Password'/>
-                    <input id='confirmPassword' type='password' placeholder='Confirm your password'/>
+                    <input id='email' type='email' placeholder='Email address' onChange={handleEmailChange}/>
+                    <input id='password' type='password' placeholder='Password' onChange={handlePasswordChange}/>
+                    <input id='confirmPassword' type='password' placeholder='Confirm your password' onChange={handleConfirmPasswordChange}/>
                     <button>Sign up</button>
 
                     <p>
