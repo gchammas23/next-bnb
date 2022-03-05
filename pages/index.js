174e32201995474887be97aadb0c941c71a63b06
Gabriel Chammas <gabrielchammas23@gmail.com>
2022-03-05 16:21:58 +0200

    Use House model in home page

--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,12 @@ import Cookies from 'cookies';
 import { useStoreActions } from 'easy-peasy';
 import { useEffect } from 'react';
 
-import houses from '../houses';
+import { House as HouseModel } from '../models';
 import House from '../components/House';
 import Layout from '../components/Layout';
 
-const content = (
-  <div>
-      <h2>Places to stay</h2>
-      <div className='houses'>
-      {
-        houses.map((house, index) => {
-          return <House key={index} {...house} />
-        })
-      }
-      </div>
-      <style jsx>{`
-        .houses {
-          display: grid;
-          grid-template-columns: 49% 49%;
-          grid-template-rows: 300px 300px;
-          grid-gap: 2%;
-        }
-      `}</style>
-    </div>
-);
 
-export default function Home({ session }) {
+export default function Home({ session, houses }) {
   const setLoggedIn = useStoreActions((actions) => actions.auth.setLoggedIn);
 
   useEffect(() => {
@@ -35,17 +15,39 @@ export default function Home({ session }) {
       setLoggedIn(true);
     }
   }, []);
-  
-  return <Layout content={content}/>
+
+  return <Layout content={
+    <div>
+      <h2>Places to stay</h2>
+      <div className='houses'>
+        {
+          houses.map((house, index) => {
+            return <House key={index} {...house} />
+          })
+        }
+      </div>
+      <style jsx>{`
+      .houses {
+        display: grid;
+        grid-template-columns: 49% 49%;
+        grid-template-rows: 300px 300px;
+        grid-gap: 2%;
+      }
+    `}</style>
+    </div>
+
+  } />
 }
 
 export async function getServerSideProps({ req, res, next }) {
+  const houses = await HouseModel.findAndCountAll();
   const cookies = new Cookies(req, res);
   const session = cookies.get('next-bnb-session');
 
   return {
     props: {
-      session: session || null
+      session: session || null,
+      houses: houses.rows.map((house) => house.dataValues);
     }
   }
 }
